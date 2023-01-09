34670c6fd686e48c2f4d5a7d4c0aa8083959a07f
Stecathw <william_sainte_catherine@hotmail.com>
2023-01-09 13:47:00 +0100

    feat(Colors): little change of color

--- lib/ui/colors.dart
+++ lib/ui/colors.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static Color primaryColor = const Color(0XFF39298a);
-  static Color primaryColorDark = const Color(0xFF231954);
+  static Color primaryColorDark = const Color(0xFF231955);
 }
